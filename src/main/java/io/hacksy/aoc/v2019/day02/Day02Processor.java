c78f0fba3cefb5d56f2a7dda327395863885d41d
greghxc <greg@hacksy.io>
2019-12-02 12:29:11 -0800

    "Hark! The herald angels!"

--- src/main/java/io/hacksy/aoc/v2019/day02/Day02Processor.java
+++ src/main/java/io/hacksy/aoc/v2019/day02/Day02Processor.java
@@ -13,8 +13,8 @@ class Day02Processor {
         while (products.hasNext()) {
             var ints = List.of(input.split(",")).map(Integer::parseInt);
             var p = products.next();
-            ints = replaceIndex(ints, 1, p._1());
-            ints = replaceIndex(ints, 2, p._2());
+            ints = ints.update(1, p._1());
+            ints = ints.update(2, p._2());
             if (doCalc(ints) == 19690720) {
                 return Integer.toString(100 * p._1() + p._2());
             }
@@ -26,10 +26,10 @@ class Day02Processor {
         for (int i = 0; i < ints.size(); i += 4) {
             switch (ints.get(i)) {
                 case 1:
-                    ints = replaceIndex(ints, ints.get(i + 3), ints.get(ints.get(i + 1)) + ints.get(ints.get(i + 2)));
+                    ints = ints.update(ints.get(i + 3), ints.get(ints.get(i + 1)) + ints.get(ints.get(i + 2)));
                     break;
                 case 2:
-                    ints = replaceIndex(ints, ints.get(i + 3), ints.get(ints.get(i + 1)) * ints.get(ints.get(i + 2)));
+                    ints = ints.update(ints.get(i + 3), ints.get(ints.get(i + 1)) * ints.get(ints.get(i + 2)));
                     break;
                 case 99:
                     return ints.get(0);
@@ -38,9 +38,5 @@ class Day02Processor {
             }
         }
         throw new RuntimeException("Does not terminate.");
-    }
-
-    private List<Integer> replaceIndex(List<Integer> integers, int index, int value) {
-        return integers.subSequence(0, index).append(value).appendAll(integers.subSequence(index + 1));
     }
 }
